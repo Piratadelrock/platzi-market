8f79ae4943c6376908e0111051396025217bf59f
Emmanuel Lopera <20448841+Piratadelrock@users.noreply.github.com>
2023-05-08 12:26:30 -0500

    added joinColumn with Producto & Categoria

--- src/main/java/com/platzi/market/persistence/entity/Categoria.java
+++ src/main/java/com/platzi/market/persistence/entity/Categoria.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @Entity
@@ -16,5 +18,9 @@ public class Categoria {
 
     private String descripcion;
     private Boolean estado;
+
+    @OneToMany(mappedBy = "categoria")
+    private List<Producto> productos;
+
 
 }
