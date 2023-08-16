9d87b57ef518e272456c31a9a4ef2013bb3816fa
Emmanuel Lopera <20448841+Piratadelrock@users.noreply.github.com>
2023-08-16 01:47:26 -0500

    update Project with crud repository, where ProductoCrudRepository is an interface and ProductoRepository the logic (service)

--- src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
@@ -3,6 +3,24 @@ package com.platzi.market.persistence.crud;
 import com.platzi.market.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto, Long> {
+
+//    @Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
+//    List<Producto> findByIdCategoria(int idCategoria);
+    //se usa query method  -> findBy, OrderBy, Asc son palabras clave,
+    // si tenemos nuestro @Query, no necesitamos nombrarlos asi,
+    // pero si es mejor practica hacerlo con jpa,querymethods para tener mas flexibilidad
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
+
+    Optional<List<Producto>> findByPrecioVentaLessThanAndIdCategoriaOrderByNombreAsc(double precioVenta, int idCategoria);
+
+    Optional<List<Producto>> findByPrecioVentaGreaterThanEqualAndCantidadStockLessThanOrderByNombre(double precioVenta, int cantidadStock);
+
+    Optional<List<Producto>> findByNombreAndPrecioVentaGreaterThanAndEstado(String nombre,double precioVenta,boolean estado);
 
 }
