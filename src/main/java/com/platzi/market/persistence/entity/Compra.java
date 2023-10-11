6dfb02ea2992a6b06c44db5b0bd25cc74450d388
Emmanuel Lopera <20448841+Piratadelrock@users.noreply.github.com>
2023-10-10 19:13:37 -0500

    add ProductController REST

--- src/main/java/com/platzi/market/persistence/entity/Compra.java
+++ src/main/java/com/platzi/market/persistence/entity/Compra.java
@@ -1,13 +1,10 @@
 package com.platzi.market.persistence.entity;
 
 import javax.persistence.*;
-import lombok.*;
-
 import java.time.LocalDateTime;
 import java.util.List;
 
 
-@Data
 @Entity
 @Table(name ="compras")
 public class Compra {
@@ -35,5 +32,67 @@ public class Compra {
     @OneToMany(mappedBy="compra")
     private List<ComprasProducto> productos;
 
+    public Integer getIdCompra() {
+        return idCompra;
+    }
 
+    public void setIdCompra(Integer idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDateTime fecha) {
+        this.fecha = fecha;
+    }
+
+    public Integer getMedioPago() {
+        return medioPago;
+    }
+
+    public void setMedioPago(Integer medioPago) {
+        this.medioPago = medioPago;
+    }
+
+    public Integer getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(Integer comentario) {
+        this.comentario = comentario;
+    }
+
+    public Integer getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Integer estado) {
+        this.estado = estado;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<ComprasProducto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<ComprasProducto> productos) {
+        this.productos = productos;
+    }
 }
