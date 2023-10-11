99f81bb2139d697fb3d47384237ab6778836e7d3
Emmanuel Lopera <20448841+Piratadelrock@users.noreply.github.com>
2023-10-10 19:13:49 -0500

    add ProductController REST

--- src/main/java/com/platzi/market/persistence/mapper/ProductMapper.java
+++ src/main/java/com/platzi/market/persistence/mapper/ProductMapper.java
@@ -7,20 +7,18 @@ import org.mapstruct.*;
 import java.util.List;
 
 @Mapper(componentModel = "spring",uses = {CategoryMapper.class})
-
 public interface ProductMapper {
     @Mappings({
             @Mapping(source = "idProducto",target="productId"),
-            @Mapping(source = "nombre",target="name"),
             @Mapping(source = "idCategoria",target="categoryId"),
+            @Mapping(source = "nombre",target="name"),
             @Mapping(source = "precioVenta",target="price"),
             @Mapping(source = "cantidadStock",target="stock"),
             @Mapping(source = "estado",target="active"),
-            @Mapping(source = "categoria",target="category"),
-
-            })
+            @Mapping(source = "categoria",target="category")
+    })
     Product toProduct(Producto producto);
-    //
+    //este hace la parte de los DTO COMO toProductDto()
     List<Product> toProducts(List<Producto> productos);
 
     @InheritInverseConfiguration
